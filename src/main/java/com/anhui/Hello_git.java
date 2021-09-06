544db8f6429345f593549a2ebcd970f51b1d31ab
zlpxiaokeai <1712223272@qq.com>
2021-09-06 09:48:55 +0800

    second commit

--- src/main/java/com/anhui/Hello_git.java
+++ src/main/java/com/anhui/Hello_git.java
@@ -3,5 +3,6 @@ package com.anhui;
 public class Hello_git {
     public static void main (String args[]){
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
